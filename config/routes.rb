5cd3f96e1987c670311c647e8b3a46978e941c25
marc thomas <marc.thomas5608@gmail.com>
2026-03-10 11:39:49 +0100

    commit pour pull en master

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,4 @@
 Rails.application.routes.draw do
-  get "toys/index"
-  get "toys/show"
-  get "toys/new"
-  get "toys/create"
-  get "toys/edit"
-  get "toys/update"
-  get "toys/destroy"
-  get "boxes/index"
-  get "boxes/show"
-  get "boxes/new"
-  get "boxes/create"
-  get "boxes/edit"
-  get "boxes/update"
-  get "boxes/destroy"
   devise_for :users
   root to: "pages#home"
   resources :boxes, only: [:index, :show, :new, :create, :edit, :update] do
