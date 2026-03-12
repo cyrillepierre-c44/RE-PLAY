38e9efef18fefe38a59e484bee66f93e7e65632f
PIERRE Cyrille <cyrille.pierre@gmail.com>
2026-03-12 14:23:46 +0100

    en cours

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,4 +1,8 @@
 class ToysController < ApplicationController
+  SYSTEM_PROMPT = "Vous êtes un expert en vente d'articles d'occasion de toutes sortes. Je travaille pour une entreprise
+qui vend des jouets d'occasion et je souhaiterais savoir à quel prix je peux les vendre. Peux-tu m'aider à trouver
+le meilleur prix d'occasion en tenant compte du prix du neuf et des prix pratiqués par la concurrence?"
+
   before_action :set_toy, only: %i[show edit update destroy]
 
   def index
@@ -8,21 +12,17 @@ class ToysController < ApplicationController
 
   def show
     authorize @toy
-    @boxes = Box.all
-    @categories = Category.all
-    @actions = Action.all
-    # @action = Action.new
-    # @action.toy_id = @toys.id
-    # @action.actionable_type = "Toy"
-    # @action.actionable_id = @toys.id
-    # @action.user_id = current_user.id
-    # @action.save
   end
 
   def new
     @toy = Toy.new
     authorize @toy
     @box = Box.find(params[:box_id])
+    @ruby_llm_chat = RubyLLM.chat(model: "gpt-4.1")
+    response = chat.ask("What is Ruby on Rails?")
+    question = @ruby_llm_chat.with_instructions(instructions)
+      @response = @ruby_llm_chat.ask(@message.content, with: with)
+    end
   end
 
   def create
