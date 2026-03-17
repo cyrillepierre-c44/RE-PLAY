03d993ba68dcf66115ca1e696758c89bd9bae899
PIERRE Cyrille <cyrille.pierre@gmail.com>
2026-03-17 15:43:59 +0100

    navbar et users ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "users/registrations" }
+  resources :users, only: [:index, :destroy]
   root to: "pages#home"
   resources :boxes, only: [:index, :show, :new, :create, :edit, :update] do
     resources :toys, only: [:new, :create]
