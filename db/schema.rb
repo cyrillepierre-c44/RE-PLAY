b4e73668b4718825be31173e479e7809ebd9f124
PIERRE Cyrille <cyrille.pierre@gmail.com>
2026-03-18 14:35:57 +0100

    scss onboarding modifié

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 ActiveRecord::Schema[8.1].define(version: 2026_03_18_085314) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
-  enable_extension "vector"
 
   create_table "actions", force: :cascade do |t|
     t.bigint "actionable_id"
@@ -58,7 +57,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_18_085314) do
     t.bigint "category_id", null: false
     t.datetime "created_at", null: false
     t.boolean "electronic"
-    t.string "status"
+    t.string "status", default: "pending", null: false
     t.datetime "updated_at", null: false
     t.decimal "weight"
     t.index ["category_id"], name: "index_boxes_on_category_id"
@@ -68,6 +67,15 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_18_085314) do
     t.datetime "created_at", null: false
     t.string "name"
     t.datetime "updated_at", null: false
+  end
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.bigint "searchable_id"
+    t.string "searchable_type"
+    t.datetime "updated_at", null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable"
   end
 
   create_table "solid_cable_messages", force: :cascade do |t|
@@ -212,9 +220,21 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_18_085314) do
     t.index ["key"], name: "index_solid_queue_semaphores_on_key", unique: true
   end
 
-# Could not dump table "toys" because of following StandardError
-#   Unknown type 'vector(1536)' for column 'embedding'
-
+  create_table "toys", force: :cascade do |t|
+    t.string "barcode"
+    t.bigint "box_id", null: false
+    t.bigint "category_id", null: false
+    t.boolean "clean"
+    t.boolean "complete"
+    t.datetime "created_at", null: false
+    t.string "location", default: "En attente de validation"
+    t.boolean "playable"
+    t.decimal "price"
+    t.string "status", default: "pending", null: false
+    t.datetime "updated_at", null: false
+    t.index ["box_id"], name: "index_toys_on_box_id"
+    t.index ["category_id"], name: "index_toys_on_category_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.boolean "admin", default: false, null: false
